67c741b9ccccb4fb221510c5edb7d8006f846080
student <student@itismeucci.com>
2026-09-23 12:33:27 +0200

    first commit

---
+++ src/main/java/com/moon/thread/Corridore.java
@@ -0,0 +1,30 @@
+package com.moon.thread;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Corridore extends Thread {
+    private String nome;
+
+    public Corridore(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void run() {
+        for (int passo = 1; passo <= 5; passo++) {
+            System.out.println(nome + " ha fatto il passo " + passo);
+
+            try {
+        
+                int pausa = ThreadLocalRandom.current().nextInt(200, 801);
+                Thread.sleep(pausa);
+            } catch (InterruptedException e) {
+                System.out.println(nome + " è stato interrotto.");
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+
+        System.out.println(nome + " ha raggiunto il traguardo!");
+    }
+}
